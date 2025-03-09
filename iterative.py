122de2ddbd992e46f0583e14c0ab8dd995d5c24c
MOHANACHARI <ma9861557@gmail.com>
2025-03-09 11:11:17 +0530

    Add files via upload
    in this consist of questions and source code of iterative statement

---
+++ iterative.py
@@ -0,0 +1,68 @@
+# # Exercise 1 : Sum of Squares
+# sum = 0
+# for i in range(1 , 6) :
+#     result = i**2
+#     sum += result
+#     print(sum)
+
+
+# # Exercise 2 : Countdown
+# count = 5
+# while count > 0 :
+#     print(count)
+#     count -= 1
+
+    
+# #  Exercise 3 : Multiplication Table with Nested For Loop
+# table = int(input("enter the table :"))
+# for i in range(1) :
+#     for j in range(1 , 11) :
+#         print(f"{table} X {j} = {table * j}")
+    
+        
+
+
+# #Write a Python program that uses a "for" loop to find the sum of all even numbers between 0 and 10 (inclusive)...4
+# sum = 0
+# for i in range(0 , 11 , 2) :
+#     sum += i
+#     print(sum)
+
+
+#Calculate the sum of all numbers from 1 to a given number....5
+sum = 0
+num = int(input("enter the number :"))
+for i in range(1 , num) :
+    sum += i
+print(sum)
+
+
+# # Display numbers from a list using loop....6
+# for i in [1 , 2 , 3 , 4 , 5] :
+#     print(f"the given numbers is {i}")
+
+# #Display numbers from 10 to 1 using for loop.......7
+# count = -10
+# while count < 0 :
+#     print(count)
+#     count += 1
+
+
+# #Write a program to display all prime numbers within a range.....8
+# a = int(input("enter the f1irst number :"))
+# b = int(input("enter the second number :"))
+# for num in range(a , b+1) :
+#     if num > 1 :
+
+#         for i in range (2 ,num) :
+#             if num % i == 0 :
+#                 break
+#         else :
+#             print(num) 
+
+
+
+
+
+
+
